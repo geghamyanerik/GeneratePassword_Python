7a30dea61e0e8203e41a409a30c92e2764d2f911
erik.geghamyanerik <erik.geghamyan@mail.ru>
2024-02-20 20:38:12 +0400

    first commit

---
+++ generatepassword.py
@@ -0,0 +1,33 @@
+import string
+import random
+
+
+
+def generatePassword(theLength):
+    letters = string.ascii_letters
+    digit = string.digits
+    totalChars = letters + digit
+    finallPassword = ""
+    for i in range(theLength):
+        finallPassword+= random.choice(totalChars)
+    return finallPassword
+        
+
+
+
+
+
+def main():
+    while True:
+       
+        theLength = int(input("Enter password length : "))
+      
+        
+        thePassword = generatePassword(theLength)
+        
+        print("Your password " +thePassword)
+       
+        break
+       
+main()
+print("Heloo")
